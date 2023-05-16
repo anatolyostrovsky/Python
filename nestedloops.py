55b807d5840dd359ae89a57ab442572ed3886063
Anatolijs Ostrovskis <94558189+anatolyostrovsky@users.noreply.github.com>
2023-05-16 20:34:40 +0100

    Add files via upload

---
+++ nestedloops.py
@@ -0,0 +1,15 @@
+x = ["big", "small", "light", "heavy", "bold"]
+y = ["iron", "silver", "gold", "diamond", "platinum"]
+
+a = 0
+b = 0
+
+for i in x:
+   
+     for j in y:
+        print(x[a],y[b])
+        b += 1
+     a += 1
+     b = 0
+        
+            
