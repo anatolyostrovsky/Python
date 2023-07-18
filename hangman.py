17a417c5a079c5a16426a4bf005b3e6d70f24423
anatolyostrovsky <anatolyostrovsky91@gmail.com>
2023-07-18 13:29:37 +0100

    second commit

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import random
 
 def hangman():
-    word = random.choice(["slice", "spice", "gross", "chemist", "totem", "poppy", "light", "spirit", "check", "bombard", "ghost", "fight", "loser", "comic"])
+    word = random.choice(["slice", "spice", "gross", "chemist", "clown", "chose", "totem", "poppy", "light", "spirit", "check", "bombard", "ghost", "fight", "loser", "comic"])
     validLetters = "abcdefghijklmnopqrstuvwxyz"
     turns = 10
     attemptmade = ""
@@ -9,13 +9,80 @@ def hangman():
     while len(word) > 0:
         main = ""
         missed = 0
+        for letter in word:
+            if letter in attemptmade:
+                main = main + letter
+            else:
+                main = main + "-" + ""
 
+        if main == word:
+            print(main)
+            print("You win!")
+            break
+        print("Guess the word: ", main)
+        guess = input()
 
-
-
+        if guess in validLetters:
+            attemptmade = attemptmade + guess
+        else:
+            print("Enter a valid character")
+            guess = input()
+        if guess not in word:
+            turns = turns -1
+            if turns == 9:
+                print("9 turns left")
+                print("------------")
+            if turns == 8:
+                print("8 turns left")
+                print("------------")
+                print("      0     ")
+            if turns == 7:
+                print("7 turns left")
+                print("------------")
+                print("      0     ")
+                print("      |     ")
+            if turns == 6:
+                print("6 turns left")
+                print("------------")
+                print("      0     ")
+                print("      |     ")
+                print("     /      ")
+            if turns == 5:
+                print("5 turns left")
+                print("------------")
+                print("      0     ")
+                print("      |     ")
+                print("     / \    ")
+            if turns == 4:
+                print("4 turns left")
+                print("------------")
+                print("      0     ")
+                print("     /|     ")
+                print("     / \    ")
+            if turns == 3:
+                print("3 turns left")
+                print("------------")
+                print("      0/    ")
+                print("     /|     ")
+                print("     / \    ")
+            if turns == 2:
+                print("2 turns left")
+                print("------------")
+                print("      0/   |")
+                print("     /|     ")
+                print("     / \    ")
+            if turns == 1:
+                print("1 turns left")
+                print("------------")
+                print("      0/   |")
+                print("     /|    |")
+                print("     / \    ")
+            if turns == 0:
+                print("Game Over")
+                break
 name = input("Enter your name")
 
-print("Welcome ", name)
+print("Welcome", name)
 print("--------------------------")
 print("Now try to guess the word in 10 turns!")
 hangman()
