d3704a30ae1c5c1389f0637852db2facc7fcc814
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2025-02-05 14:17:02 +0000

    flask-app-update2

--- flask-app/flask-app.py
+++ flask-app/flask-app.py
@@ -7,5 +7,9 @@ application = Flask(__name__)
 def index():
     return "Hello World by Flask"
 
+@application.route("/help")
+def help():
+    return "This ia a help page!"
+
 if __name__== "__main__":
     application.run()
