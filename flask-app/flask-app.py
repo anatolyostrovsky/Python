ceddfb02b7543ec2ad030769ec249bcbbe45ea1b
Anatoly Ostrovsky <anatolyostrovsky91@gmail.com>
2025-02-06 15:44:17 +0000

    app update

--- flask-app/flask-app.py
+++ flask-app/flask-app.py
@@ -11,5 +11,28 @@ def index():
 def help():
     return "This ia a help page!"
 
+@application.route("/user")
+def user():
+    return "This ia a user page!"
+
+@application.route("/user/<username>")
+def showuserpage(username):
+    return "Hello " + username.upper()
+
+@application.route("/path/<path:subpath>")
+def print_subpath(subpath):
+    return "Subpath is: " + subpath
+
+@application.route("/square/<int:x>") 
+def count_square(x):
+    return "Your number Squared is: " + str(x * x)
+
+@application.route("/travel")
+def show_html_page():
+    myfile = open("travel.html", mode = 'r')
+    page = myfile.read()
+
+    return page
+
 if __name__== "__main__":
     application.run()
